ca38c05288b5dccefde9beb3ba670bd263202384
Peter Hoang <peter.hoang@soti.net>
2018-03-06 14:28:50 -0500

    initial commit of react learning

---
+++ app/containers/App/actions.js
@@ -0,0 +1,61 @@
+/*
+ * App Actions
+ *
+ * Actions change things in your application
+ * Since this boilerplate uses a uni-directional data flow, specifically redux,
+ * we have these actions which are the only way your application interacts with
+ * your application state. This guarantees that your state is up to date and nobody
+ * messes it up weirdly somewhere.
+ *
+ * To add a new Action:
+ * 1) Import your constant
+ * 2) Add a function like this:
+ *    export function yourAction(var) {
+ *        return { type: YOUR_ACTION_CONSTANT, var: var }
+ *    }
+ */
+
+import {
+  LOAD_CHARACTERS,
+  LOAD_CHARACTERS_SUCCESS,
+  LOAD_CHARACTERS_ERROR,
+} from './constants';
+
+/**
+ * Load the repositories, this action starts the request saga
+ *
+ * @return {object} An action object with a type of LOAD_REPOS
+ */
+export function loadCharacters() {
+  return {
+    type: LOAD_CHARACTERS,
+  };
+}
+
+/**
+ * Dispatched when the repositories are loaded by the request saga
+ *
+ * @param  {array} characters The repository data
+ *
+ * @return {object}      An action object with a type of LOAD_REPOS_SUCCESS passing the repos
+ */
+export function charactersLoaded(characters) {
+  return {
+    type: LOAD_CHARACTERS_SUCCESS,
+    characters,
+  };
+}
+
+/**
+ * Dispatched when loading the repositories fails
+ *
+ * @param  {object} error The error
+ *
+ * @return {object}       An action object with a type of LOAD_REPOS_ERROR passing the error
+ */
+export function charactersLoadingError(error) {
+  return {
+    type: LOAD_REPOS_ERROR,
+    error,
+  };
+}
